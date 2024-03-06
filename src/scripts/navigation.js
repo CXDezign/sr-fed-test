2948dce6b3538722fb2f6b4fbacd9373d313b475
CXDezign <contact@cxdezign.dev>
2024-03-06 17:45:40 +0100

    Navigation Transitioning Nightmare

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -8,13 +8,37 @@ const toggle = navigation?.desktop?.querySelector('.navigation-toggle')
 let menu = false
 
 const toggleNavigation = () => {
+	const navigationComputedStyle = window.getComputedStyle(navigation.mobile)
+	navigation.mobile.classList.add('transitioning')
+	let transition = navigationComputedStyle?.getPropertyValue('transition-duration')
+	transition = transition.replace('s', '')
+
+	setTimeout(() => {
+		navigation.mobile.classList.remove('transitioning')
+		toggle.disabled = false
+	}, transition * 1000)
+	
 	isOverlayActive(menu)
-	menu ? navigation?.mobile?.classList.add('active') : navigation?.mobile?.classList.remove('active')
 	menu ? toggle?.classList.add('active') : toggle?.classList.remove('active')
+	menu ? navigation?.mobile?.classList.add('active') : navigation?.mobile?.classList.remove('active')
 }
 
-toggle?.addEventListener('click', () => {
+const disableNavigation = () => {
 	menu = !menu
+	toggle.disabled = true
+}
+
+window.addEventListener('resize', () => {
+	const windowWidth = window.innerWidth
+	if (windowWidth > 768 & menu === true) {
+		console.log('window size:', windowWidth)
+		disableNavigation()
+		toggleNavigation()
+	}
+})
+
+toggle?.addEventListener('click', () => {
+	disableNavigation()
 	toggleNavigation()
 })
 
