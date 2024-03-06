26abc018cd42c80a6e9fc2a921a8538a8f8c6035
CXDezign <contact@cxdezign.dev>
2024-03-06 19:36:20 +0100

    Valid HTML Fixes

--- src/scripts/dialog.js
+++ src/scripts/dialog.js
@@ -3,11 +3,15 @@ import { isOverlayActive } from './overlays'
 const dialogButtons = document.querySelectorAll('.dialog-button-open')
 
 dialogButtons.forEach((button) => {
-	const dialogTarget = button.getAttribute('dialog-target')
+	const dialogTarget = button.dataset.dialogTarget
 	const dialog = document.querySelector(`#${dialogTarget}`)
+	const buttonClose = dialog?.querySelector('button.dialog-button-close')
 	button.addEventListener('click', () => {
 		dialog.showModal()
 		isOverlayActive(true)
+	})
+	buttonClose.addEventListener('click', () => {
+		dialog.close()
 	})
 	dialog.addEventListener('close', () => isOverlayActive(false))
 })
